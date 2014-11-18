8955626dbc0cf88972c1bd7440b4a8b570578001
Stino <Linksonder@gmail.com>
2014-11-18 14:38:12 +0100

    Initial Create

---
+++ MusicCollectionMVVMLight/ViewModel/SongListViewModel.cs
@@ -0,0 +1,53 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MusicCollectionMVVMLight.Model;
+using MusicCollectionMVVMMVVMLight.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Linq;
+
+namespace MusicCollectionMVVMLight.ViewModel
+{
+    public class SongListViewModel : ViewModelBase
+    {
+        ISongRepository songRepository;
+
+        public SongListViewModel()
+        {
+            songRepository = new DummySongRepository();
+            var songList = songRepository.ToList().Select(s => new SongViewModel(s));
+
+
+            AddSong = new RelayCommand(AddNewSong, CanAddNewSong);
+            Song = new SongViewModel();
+            Songs = new ObservableCollection<SongViewModel>(songList);
+        }
+
+        private void AddNewSong()
+        {
+            Songs.Add(Song);
+        }
+
+        private bool CanAddNewSong()
+        {
+            if (Song == null)
+                return false;
+
+            if (Song.Id <= 0)
+                return false;
+
+            if (String.IsNullOrEmpty(Song.Artist) || String.IsNullOrEmpty(Song.Title))
+                return false;
+
+            return true;
+        }
+
+
+        public ObservableCollection<SongViewModel> Songs { get; set; }
+
+        public SongViewModel Song { get; set; }
+
+        public ICommand AddSong { get; set; }
+    }
+}
